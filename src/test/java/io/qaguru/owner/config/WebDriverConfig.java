fda8f2f93b56c29954ce69cb39fa21145a9ca22e
Your NameDenis <tdv14011984@gmail.com>
2022-06-28 18:07:48 +0300

    add test

--- src/test/java/io/qaguru/owner/config/WebDriverConfig.java
+++ src/test/java/io/qaguru/owner/config/WebDriverConfig.java
@@ -2,11 +2,12 @@ package io.qaguru.owner.config;
 
 public class WebDriverConfig {
 
-    public Browser getBrowser(){
-        return Browser.CHROME;
+    public String getBaseUrl(){
+        System.getProperties("");
+        return "https://github.com/";
     }
 
-    public String getBaseUrl(){
-        return "https://github.com/";
+    public Browser getBrowser() {
+        return Browser.CHROME;
     }
 }
